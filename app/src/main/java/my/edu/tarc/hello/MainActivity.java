3da4fb3e44bd6d72c6ee409634ca5cd1b9f77e73
tanhanzong <hztan-wa16@student.tarc.edu.my>
2018-10-18 11:58:00 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/hello/MainActivity.java
@@ -0,0 +1,34 @@
+package my.edu.tarc.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewMsg; //TextView = class, textViewMsg = instance
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //onCreate() = main() function
+        super.onCreate(savedInstanceState);
+        //display UI
+        //R = resource
+        //layout = folder
+        setContentView(R.layout.activity_main);
+
+
+
+        //Link UI to program
+        textViewMsg = findViewById(R.id.textViewMessage);
+
+    }
+
+    public void showMessage(View view){
+        textViewMsg.setText("Hello, hanzong!");
+    }
+
+    public void clearMessage(View view){
+        textViewMsg.setText("");
+    }
+}
